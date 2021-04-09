51b6ab8a1d624b25295ba653bbec608398de350a
gustavo-sorati <gustavo.sorati@outlook.com>
2021-04-08 23:06:49 -0300

    aula 3 finished

---
+++ src/controllers/DashboardController.js
@@ -0,0 +1,40 @@
+const Job = require('../models/Job');
+const Profile = require('../models/Profile');
+
+const JobUtils = require('../utils/JobUtils');
+
+module.exports = {
+  index(req, res) {
+    const jobs = Job.get();
+    const profile = Profile.get();
+
+    let statusCount = {
+      progress: 0,
+      done: 0,
+      total: jobs.length
+    }
+
+    let jobTotalHours = 0;
+
+    const updatedJobs = jobs.map(job => {
+      const remaining = JobUtils.remainingDays(job);
+      const status = remaining <= 0 ? 'done' : 'progress';
+
+      statusCount[status] += 1;
+
+      jobTotalHours = status === 'progress' ? jobTotalHours + Number(job['daily-hours']) : jobTotalHours;
+
+      return {
+        ...job,
+        remaining,
+        status,
+        budget: JobUtils.calculateBudget(job, profile['value-hour']),
+      };
+    })
+
+    // qtde de horas q quero trabalhar MENOS quantidade de horas/dia de cada job em progress
+    const freeHours = profile['hours-per-day'] - jobTotalHours;
+
+    return res.render('index', { jobs: updatedJobs, profile, statusCount, freeHours });
+  }
+}
